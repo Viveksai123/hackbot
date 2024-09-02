2fd4a839bc868d9a4956b31bb2cc9ad7b7543c9b
Viveksai123 <periketiviveksai04@gmail.com>
2024-09-02 22:47:56 +0530

    f

--- src/components/Levelone.js
+++ src/components/Levelone.js
@@ -242,8 +242,8 @@ const Levelone = ({ username, rollnum, initialScore, timeLeft }) => {
                 </p>
                 
                 <br/>
-                <div className="input-wrapper">
-                  <div className='input-wrapper1'>
+                
+                  <div className='input-wrapper2'>
                     <input
                       className="password-input"
                       type="text"
@@ -253,7 +253,7 @@ const Levelone = ({ username, rollnum, initialScore, timeLeft }) => {
                     />
                     
                     <GiLightningStorm onClick={handleCastSpell} className="cast-spell-icon" />
-                  </div>
+                 
                 </div>
               </div>
             )}
