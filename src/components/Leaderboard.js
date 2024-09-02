ded6782d0ba3c6c56d42f1fbba70e755e1a9aaa4
Viveksai123 <periketiviveksai04@gmail.com>
2024-09-02 23:08:31 +0530

    f

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrophy } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faTrophy } from '@fortawesome/free-solid-svg-icons';
 import './styles/Leaderboard.css'; // Ensure this path is correct
 
 const Leaderboard = () => {
